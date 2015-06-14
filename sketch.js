0f68ab5cf3cefb939c4999449a0caae78b19e33f
KaroAntonio <karoantonio@gmail.com>
2015-06-13 22:32:47 -0600

    Full Width

--- sketch.js
+++ sketch.js
@@ -24,32 +24,26 @@ function setup() {
 }
 
 function draw() {
-    var spectrum = fft.analyze();
-    //fill(color(204, 255, spectrum[0]));
-    //background(color((spectrum[0] + 140)%255, 255, 255));
-    background(color(125, 0, 0, 70));
-    //background('blue');
-    for (j = 0; j < 10; j++) {
-        beginShape();
-        for (i = 0; i<(spectrum.length/30); i++) {
-           //stroke(color(spectrum[i + (j * 30)]%255, 200, 255));
-            stroke(color(spectrum[i + (j * 30)]%255, 0, 255));
-           //vertex(i * width/(spectrum.length/3), map(spectrum[i], 0, height, height, 0) );
-           vertex(((i) * width/(spectrum.length/10)) + width/(3),
-                 (height - height/10 * (j + 1)) - spectrum[i + (j * 30)]);
+    if (song.isPlaying) {
+        
+        var spectrum = fft.analyze();
+        //fill(color(204, 255, spectrum[0]));
+        //background(color((spectrum[0] + 140)%255, 255, 255));
+        background(color(125, 0, 0, 70));
+        //background('blue');
+        for (j = 0; j < 10; j++) {
+            beginShape();
+            for (i = 0; i<(spectrum.length/30); i++) {
+               //stroke(color(spectrum[i + (j * 30)]%255, 200, 255));
+                stroke(color(spectrum[i + (j * 30)]%255, 0, 255));
+               //vertex(i * width/(spectrum.length/3), map(spectrum[i], 0, height, height, 0) );
+               vertex(((i) * width/(spectrum.length/30)),
+                     (height - height/10 * (j + 1)) - spectrum[i + (j * 30)]/2);
+            }
+            endShape();
         }
-        endShape();
-    }
    
-     /*
-    for (i = 0; i < spectrum.length; i++) {
-        stroke(color(i%255, 255, 255));
-        ellipse(width/2,
-                height/2,
-                spectrum[i]*2,
-                spectrum[i]*2)
     }
-*/
 }
 
 function mousePressed() {
